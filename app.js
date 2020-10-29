a5e633c7018e11f59842ebf1ec8bac24a70bc791
Sunitha Mannam <mannam.sunitha@gmail.com>
2020-10-29 21:22:02 +0100

    env changed

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
-    // origin: process.env.ORIGIN,
+    // origin: "http://localhost:3000",
+    origin: process.env.ORIGIN,
   })
 );
 
